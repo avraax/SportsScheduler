a033bd3d39f91e633b213572638bb87aa2dd39ff
Allan Brink Vraa <allan@mondohunter.dk>
2014-12-22 00:15:08 +0100

    Looking up soccermatches

--- GenrePage.xaml.cs
+++ GenrePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using SportsScheduler.Scraper;
 
 namespace SportsScheduler
 {	
@@ -8,17 +9,22 @@ namespace SportsScheduler
 	{	
 		public GenrePage (Genre genre)
 		{
+			var soccerEvents = new Service ().GetSoccerEvents ();
+
+			var listView = new ListView
+			{
+				RowHeight = 40
+			};
+
+			listView.ItemsSource = soccerEvents;
+			listView.ItemTemplate = new DataTemplate(typeof(TextCell));
+			listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Title");
+
+
 			Content = new StackLayout
 			{
 				Spacing = 10,
-				Children = { 
-					new Label
-					{
-						Text = genre.Title,
-						BackgroundColor = Color.Red,
-						Font = Font.SystemFontOfSize (20)
-					}
-				}
+				Children = { listView }
 			};
 		}
 	}
---
+++ Models/SoccerEventDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SportsScheduler
+{
+	public class SoccerEventDetails
+	{
+		public string HomeTeam { get; set; }
+		public string AwayTeam { get; set; }
+
+		public SoccerEventDetails ()
+		{
+		}
+	}
+}
