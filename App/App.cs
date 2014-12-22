13beb61c3034e47e2b9a6b9ae10a2ec7e3414591
Allan Brink Vraa <allan@mondohunter.dk>
2014-12-22 09:08:39 +0100

    Testing gitignore

---
+++ App/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SportsScheduler
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			return new NavigationPage(new GenreOverview());
+		}
+	}
+}
+
---
+++ App/Models/SoccerEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SportsScheduler
+{
+	public class SoccerEvent
+	{
+		public string Title { get; set; }
+
+		public SoccerEvent ()
+		{
+		}
+	}
+}
